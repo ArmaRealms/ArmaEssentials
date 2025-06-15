50592a058c9b1a40cb01b7e1713c45906cbb5933
ThiagoROX <thiagokenis@live.com>
2025-06-15 11:36:02 -0300

    Add silent option to giveKits command and improve argument handling (#32)
    * Add silent option to giveKits command and improve argument handling
    
    * Add silent mode to kit giving command

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandkit.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandkit.java
@@ -7,6 +7,7 @@ import com.earth2me.essentials.utils.AdventureUtil;
 import com.earth2me.essentials.utils.StringUtil;
 import net.ess3.api.TranslatableException;
 import org.bukkit.Server;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,37 +21,47 @@ public class Commandkit extends EssentialsCommand {
     }
 
     @Override
-    public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception {
+    public void run(final Server server, final User user, final String commandLabel, final String @NotNull [] args) throws Exception {
+        boolean silent = false;
         if (args.length < 1) {
             final String kitList = ess.getKits().listKits(ess, user);
-            user.sendTl(kitList.length() > 0 ? "kits" : "noKits", AdventureUtil.parsed(kitList));
+            user.sendTl(!kitList.isEmpty() ? "kits" : "noKits", AdventureUtil.parsed(kitList));
             throw new NoChargeException();
         } else if (args.length > 1 && user.isAuthorized("essentials.kit.others")) {
-            giveKits(getPlayer(server, user, args, 1), user, StringUtil.sanitizeString(args[0].toLowerCase(Locale.ENGLISH)).trim());
+            if (args.length >= 3 && args[2].equalsIgnoreCase("-s")) {
+                silent = true;
+            }
+            giveKits(getPlayer(server, user, args, 1), user, StringUtil.sanitizeString(args[0].toLowerCase(Locale.ENGLISH)).trim(), silent);
         } else {
-            giveKits(user, user, StringUtil.sanitizeString(args[0].toLowerCase(Locale.ENGLISH)).trim());
+            giveKits(user, user, StringUtil.sanitizeString(args[0].toLowerCase(Locale.ENGLISH)).trim(), silent);
         }
     }
 
     @Override
-    public void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
+    public void run(final Server server, final CommandSource sender, final String commandLabel, final String @NotNull [] args) throws Exception {
+        boolean silent = false;
         if (args.length < 2) {
             final String kitList = ess.getKits().listKits(ess, null);
-            sender.sendTl(kitList.length() > 0 ? "kits" : "noKits", AdventureUtil.parsed(kitList));
+            sender.sendTl(!kitList.isEmpty() ? "kits" : "noKits", AdventureUtil.parsed(kitList));
             throw new NoChargeException();
         } else {
             final User userTo = getPlayer(server, args, 1, true, false);
+            if (args.length >= 3 && args[2].equalsIgnoreCase("-s")) {
+                silent = true;
+            }
 
             for (final String kitName : args[0].toLowerCase(Locale.ENGLISH).split(",")) {
                 new Kit(kitName, ess).expandItems(userTo);
 
                 sender.sendTl("kitGiveTo", kitName, userTo.getDisplayName());
-                userTo.sendTl("kitReceive", kitName);
+                if (!silent) {
+                    sender.sendTl("kitReceive", kitName);
+                }
             }
         }
     }
 
-    private void giveKits(final User userTo, final User userFrom, final String kitNames) throws Exception {
+    private void giveKits(final User userTo, final User userFrom, final @NotNull String kitNames, final boolean silent) throws Exception {
         if (kitNames.isEmpty()) {
             throw new TranslatableException("kitNotFound");
         }
@@ -83,8 +94,9 @@ public class Commandkit extends EssentialsCommand {
                     userFrom.sendTl("kitGiveTo", kit.getName(), userTo.getDisplayName());
                 }
 
-                userTo.sendTl("kitReceive", kit.getName());
-
+                if (!silent) {
+                    userTo.sendTl("kitReceive", kit.getName());
+                }
             } catch (final NoChargeException ex) {
                 if (ess.getSettings().isDebug()) {
                     ess.getLogger().log(Level.INFO, "Soft kit error, abort spawning " + kit.getName(), ex);
@@ -96,7 +108,7 @@ public class Commandkit extends EssentialsCommand {
     }
 
     @Override
-    protected List<String> getTabCompleteOptions(final Server server, final User user, final String commandLabel, final String[] args) {
+    protected List<String> getTabCompleteOptions(final Server server, final User user, final String commandLabel, final String @NotNull [] args) {
         if (args.length == 1) {
             final List<String> options = new ArrayList<>();
             // TODO: Move all of this to its own method
@@ -109,17 +121,26 @@ public class Commandkit extends EssentialsCommand {
             return options;
         } else if (args.length == 2 && user.isAuthorized("essentials.kit.others")) {
             return getPlayers(server, user);
+        } else if (args.length == 3 && user.isAuthorized("essentials.kit.others")) {
+            final List<String> options = new ArrayList<>();
+            options.add("-s");
+            return options;
         } else {
             return Collections.emptyList();
         }
     }
 
     @Override
-    protected List<String> getTabCompleteOptions(final Server server, final CommandSource sender, final String commandLabel, final String[] args) {
+    protected List<String> getTabCompleteOptions(final Server server, final CommandSource sender, final String commandLabel, final String @NotNull [] args) {
         if (args.length == 1) {
-            return new ArrayList<>(ess.getKits().getKitKeys()); // TODO: Move this to its own method
+            // TODO: Move this to its own method
+            return new ArrayList<>(ess.getKits().getKitKeys());
         } else if (args.length == 2) {
             return getPlayers(server, sender);
+        } else if (args.length == 3 && sender.isAuthorized("essentials.kit.others.silent")) {
+            final List<String> options = new ArrayList<>();
+            options.add("-s");
+            return options;
         } else {
             return Collections.emptyList();
         }
