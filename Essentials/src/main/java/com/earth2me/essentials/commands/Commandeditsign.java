0660e83b9bf99c568cb8cd7ec20022aa666b5c30
ThiagoROX <thiagokenis@live.com>
2025-07-15 23:09:38 -0300

    Refactor sign edit commands to English and cleanup
    Replaces Portuguese command keywords with English equivalents (set, clear, copy, paste) and updates tab completion accordingly. Inlines and refactors sign direction logic, removing the now-unnecessary getBlockFace method.

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandeditsign.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandeditsign.java
@@ -19,7 +19,6 @@ import org.bukkit.block.data.type.WallSign;
 import org.bukkit.block.sign.Side;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.util.Vector;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
@@ -41,7 +40,7 @@ public class Commandeditsign extends EssentialsCommand {
         }
         final ModifiableSign sign = wrapSign((Sign) target.getState(), user);
         try {
-            if (args[0].equalsIgnoreCase("definir") && args.length > 2) {
+            if (args[0].equalsIgnoreCase("set") && args.length > 2) {
                 final String[] existingLines = sign.getLines();
                 final int line = Integer.parseInt(args[1]) - 1;
                 final String text = FormatUtil.formatString(user, "essentials.editsign", getFinalArg(args, 2)).trim();
@@ -54,9 +53,9 @@ public class Commandeditsign extends EssentialsCommand {
                 }
 
                 user.sendTl("editsignCommandSetSuccess", line + 1, text);
-            } else if (args[0].equalsIgnoreCase("limpar")) {
-                final String[] existingLines = sign.getLines();
+            } else if (args[0].equalsIgnoreCase("clear")) {
                 if (args.length == 1) {
+                    final String[] existingLines = sign.getLines();
                     for (int i = 0; i < 4; i++) { // A whole one line of line savings!
                         existingLines[i] = "";
                     }
@@ -67,6 +66,7 @@ public class Commandeditsign extends EssentialsCommand {
 
                     user.sendTl("editsignCommandClear");
                 } else {
+                    final String[] existingLines = sign.getLines();
                     final int line = Integer.parseInt(args[1]) - 1;
                     existingLines[line] = "";
 
@@ -76,12 +76,9 @@ public class Commandeditsign extends EssentialsCommand {
 
                     user.sendTl("editsignCommandClearLine", line + 1);
                 }
-            } else if (args[0].equalsIgnoreCase("copiar")) {
-                if (callSignEvent(sign, user, sign.getLines())) {
-                    return;
-                }
-
+            } else if (args[0].equalsIgnoreCase("copy")) {
                 final int line = args.length == 1 ? -1 : Integer.parseInt(args[1]) - 1;
+
                 if (line == -1) {
                     for (int i = 0; i < 4; i++) {
                         // We use unformat here to prevent players from copying signs with colors that they do not have permission to use.
@@ -94,11 +91,10 @@ public class Commandeditsign extends EssentialsCommand {
                     user.sendTl("editsignCopyLine", line + 1, commandLabel);
                 }
 
-            } else if (args[0].equalsIgnoreCase("colar")) {
+            } else if (args[0].equalsIgnoreCase("paste")) {
                 final int line = args.length == 1 ? -1 : Integer.parseInt(args[1]) - 1;
 
                 final String[] existingLines = sign.getLines();
-
                 if (line == -1) {
                     for (int i = 0; i < 4; i++) {
                         existingLines[i] = FormatUtil.formatString(user, "essentials.editsign", user.getSignCopy().get(i));
@@ -114,7 +110,6 @@ public class Commandeditsign extends EssentialsCommand {
                     }
                     user.sendTl("editsignPasteLine", line + 1, commandLabel);
                 }
-
             } else {
                 throw new NotEnoughArgumentsException();
             }
@@ -131,6 +126,7 @@ public class Commandeditsign extends EssentialsCommand {
             }
             event = new SignChangeEvent(sign.getBlock(), user.getBase(), lines, sign.isFront() ? Side.FRONT : Side.BACK);
         } else {
+            //noinspection deprecation
             event = new SignChangeEvent(sign.getBlock(), user.getBase(), lines);
         }
 
@@ -152,10 +148,10 @@ public class Commandeditsign extends EssentialsCommand {
     @Override
     protected List<String> getTabCompleteOptions(final Server server, final User user, final String commandLabel, final String[] args) {
         if (args.length == 1) {
-            return Lists.newArrayList("definir", "limpar", "copiar", "colar");
+            return Lists.newArrayList("set", "clear", "copy", "paste");
         } else if (args.length == 2) {
             return Lists.newArrayList("1", "2", "3", "4");
-        } else if (args.length == 3 && args[0].equalsIgnoreCase("definir") && NumberUtil.isPositiveInt(args[1])) {
+        } else if (args.length == 3 && args[0].equalsIgnoreCase("set") && NumberUtil.isPositiveInt(args[1])) {
             final int line = Integer.parseInt(args[1]);
             final Block target = user.getTargetBlock(5);
             if (target.getState() instanceof Sign && line <= 4) {
@@ -171,7 +167,20 @@ public class Commandeditsign extends EssentialsCommand {
     private ModifiableSign wrapSign(final Sign sign, final User user) {
         if (VersionUtil.getServerBukkitVersion().isHigherThanOrEqualTo(VersionUtil.v1_20_1_R01)) {
             final Vector eyeLocLessSign = user.getBase().getEyeLocation().toVector().subtract(sign.getLocation().add(.5, .5, .5).toVector());
-            final BlockFace signDirection = getBlockFace(sign);
+            final BlockData signBlockData = sign.getBlockData();
+
+            final BlockFace signDirection;
+            if (signBlockData instanceof org.bukkit.block.data.type.Sign) {
+                signDirection = ((org.bukkit.block.data.type.Sign) signBlockData).getRotation();
+            } else if (signBlockData instanceof WallSign) {
+                signDirection = ((WallSign) signBlockData).getFacing();
+            } else if (signBlockData instanceof HangingSign) {
+                signDirection = ((HangingSign) signBlockData).getRotation();
+            } else if (signBlockData instanceof WallHangingSign) {
+                signDirection = ((WallHangingSign) signBlockData).getFacing();
+            } else {
+                throw new IllegalStateException("Unknown block data for sign: " + signBlockData.getClass());
+            }
 
             final Side side = eyeLocLessSign.dot(signDirection.getDirection()) > 0 ? Side.FRONT : Side.BACK;
 
@@ -228,24 +237,6 @@ public class Commandeditsign extends EssentialsCommand {
                 return false;
             }
         };
-    }
-
-    private static @NotNull BlockFace getBlockFace(final Sign sign) {
-        final BlockData signBlockData = sign.getBlockData();
-
-        final BlockFace signDirection;
-        if (signBlockData instanceof final org.bukkit.block.data.type.Sign bukkitSign) {
-            signDirection = bukkitSign.getRotation();
-        } else if (signBlockData instanceof final WallSign wallSign) {
-            signDirection = wallSign.getFacing();
-        } else if (signBlockData instanceof final HangingSign hangingSign) {
-            signDirection = hangingSign.getRotation();
-        } else if (signBlockData instanceof final WallHangingSign wallHangingSign) {
-            signDirection = wallHangingSign.getFacing();
-        } else {
-            throw new IllegalStateException("Unknown block data for sign: " + signBlockData.getClass());
-        }
-        return signDirection;
     }
 
     private abstract static class ModifiableSign {
