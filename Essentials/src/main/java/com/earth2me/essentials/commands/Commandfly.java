68280905536ccbef8d53fed5c6104708b861426d
ThiagoROX <thiagokenis@live.com>
2025-11-27 18:01:33 -0300

    Refactor fly mode messaging and update message keys
    Split the fly mode message into separate keys for self and others in messages.properties. Updated Commandfly.java to use the new flyModeOther key for notifying senders when toggling fly mode for another player.

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandfly.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandfly.java
@@ -5,7 +5,6 @@ import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.CommonPlaceholders;
 import net.ess3.api.events.FlyStatusChangeEvent;
 import org.bukkit.Server;
-import org.bukkit.entity.Player;
 
 public class Commandfly extends EssentialsToggleCommand {
     public Commandfly() {
@@ -40,7 +39,7 @@ public class Commandfly extends EssentialsToggleCommand {
             }
 
             user.sendTl("flyMode", CommonPlaceholders.enableDisable(user.getSource(), enabled), user.getDisplayName());
-            if (!sender.isPlayer() || sender instanceof Player player && !player.getUniqueId().equals(user.getBase().getUniqueId())) {
+            if (!sender.isPlayer() || sender.getPlayer() != null && !sender.getPlayer().equals(user.getBase())) {
                 sender.sendTl("flyModeOther", CommonPlaceholders.enableDisable(user.getSource(), enabled), user.getDisplayName());
             }
         }
